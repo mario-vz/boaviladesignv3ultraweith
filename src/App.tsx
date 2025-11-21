d9c2a56c7e816a9f54603075c22db02f80974d99
Emeleka_build <mariovieitez@gmail.com>
2025-11-22 00:54:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ function App() {
         <section
           className="h-[90vh] w-full bg-cover bg-center bg-no-repeat relative"
           style={{
-            backgroundImage: "url('https://i.imgur.com/TU_IMAGEN.jpg')",
+            backgroundImage: "url('https://i.imgur.com/dU9xq3A.jpeg')",
           }}
         >
           {/* capa oscura para mejorar contraste */}
