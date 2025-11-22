d984fcd7ee3414775b4b8b15403c344564aac877
Emeleka_build <mariovieitez@gmail.com>
2025-11-22 02:00:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -143,7 +143,7 @@ function App() {
 
       <main className="pt-20">
         <section
-          className="h-[calc(100vh-88px)] w-full flex items-center justify-center px-6 bg-cover bg-center bg-no-repeat"
+          className="h-screen w-full flex items-center justify-center px-6 bg-cover bg-center bg-no-repeat -mt-[88px]"
           style={{
             backgroundImage: "url('https://i.imgur.com/6GgAjvA.jpeg')",
           }}
